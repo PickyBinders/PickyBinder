76fd6eb9ef00ab79b0b3f4e55ffd8095c871370d
Michele Leemann <michele_leemann@hotmail.com>
2023-02-20 14:33:46 +0100

    use mol2 file if available when praparation from sdf fails

--- bin/prepare_ligand_sdf.py
+++ bin/prepare_ligand_sdf.py
@@ -1,40 +1,115 @@
 #!/usr/bin/env python
 
-from ligand_utility import mol_to_smiles, smiles_to_3d_mol
+from ligand_utility import smiles_to_3d_mol
 from pathlib import Path
 import sys
 import shutil
 import re
+from rdkit import Chem
+from rdkit import rdBase
+from io import StringIO
 
 ref_sdf_files = sys.argv[1:]
 
 for ref in ref_sdf_files:
+    print("now processing: " + ref)
+
+    rdBase.LogToPythonStderr()
+    stderr = sys.stderr
+    sio = sys.stderr = StringIO()
+
     pattern = re.compile("(__)")
+
     if bool(re.search(pattern, ref)):
-        ligand = ref.split("__")[1].split("_")[0] + ".sdf"
-        ligand_resnum = ref.split("__")[1]
+        ligand = ref.split("__")[1].split("_")[0]
+        ligand_sdf_name = ligand + ".sdf"
+        ligand_sdf_resnum_name = ref.split("__")[1]
+        ligand_mol2_name = ligand + ".mol2"
 
-        ligand_sdf = Path() / f"{ligand}"
-        ligand_resnum_sdf = Path() / f"{ligand_resnum}"
+        sdf_file = Path() / f"{ligand_sdf_name}"
+        resnum_sdf_file = Path() / f"{ligand_sdf_resnum_name}"
+        mol2_file = Path() / f"{ligand_mol2_name}"
 
-        try:
-            if not ligand_sdf.exists():
-                smiles = mol_to_smiles(ref)
-                smiles_to_3d_mol(smiles, ligand)
-                shutil.copy(ligand_sdf, ligand_resnum_sdf)
+        if not sdf_file.exists():
+            print("here 1")
+            mol = Chem.MolFromMolFile(ref, sanitize=False)
+            problem = False
 
+            try:
+                Chem.SanitizeMol(mol)
+                mol = Chem.RemoveHs(mol)
+                smiles = Chem.MolToSmiles(mol)
+            except Exception as e:
+                print("ligand preparation using sdf file failed")
+                print("error: " + str(e))
+                problem = True
+
+            if problem and mol2_file.exists():
+                print("try to use mol2 file for ligand preparation")
+                mol = Chem.MolFromMol2File(ligand_mol2_name, sanitize=False)
+                problem = False
+                try:
+                    Chem.SanitizeMol(mol)
+                    mol = Chem.RemoveHs(mol)
+                    smiles = Chem.MolToSmiles(mol)
+                    problem = False
+                except Exception as e:
+                    print("ligand preparation using mol2 file failed")
+                    print("error: " + str(e))
+                    problem = True
+
+            if not problem:
+                smiles_to_3d_mol(smiles, ligand_sdf_name)
+                shutil.copy(sdf_file, resnum_sdf_file)
+                print("ligand preparation done")
             else:
-                shutil.copy(ligand_sdf, ligand_resnum_sdf)
+                print(ref + " ligand preparation failed")
+        else:
+            print("here 2")
+            shutil.copy(sdf_file, resnum_sdf_file)
 
-        except Exception as e:
-            print(ligand, e)
     else:
-        ligand = ref
-        ligand_preped = ligand.split(".")[0] + "_preped.sdf"
-        ligand_sdf = Path() / f"{ligand_preped}"
-        try:
-            if not ligand_sdf.exists():
-                smiles = mol_to_smiles(ref)
+        ligand = ref.split(".sdf")[0]
+        ligand_sdf_name = ref
+        ligand_preped = ligand + "_preped.sdf"
+        ligand_mol2_name = ligand + ".mol2"
+
+        sdf_file = Path() / f"{ligand_preped}"
+        mol2_file = Path() / f"{ligand_mol2_name}"
+
+        if not sdf_file.exists():
+            mol = Chem.MolFromMolFile(ref, sanitize=False)
+            problem = False
+
+            try:
+                Chem.SanitizeMol(mol)
+                mol = Chem.RemoveHs(mol)
+                smiles = Chem.MolToSmiles(mol)
+            except Exception as e:
+                print("ligand preparation using sdf file failed")
+                print("error: " + str(e))
+                problem = True
+
+            if problem and mol2_file.exists():
+                print("try to use mol2 file for ligand preparation")
+                mol = Chem.MolFromMol2File(ligand_mol2_name, sanitize=False)
+                problem = False
+                try:
+                    Chem.SanitizeMol(mol)
+                    mol = Chem.RemoveHs(mol)
+                    smiles = Chem.MolToSmiles(mol)
+                    problem = False
+                except Exception as e:
+                    print("ligand preparation using mol2 file failed")
+                    print("error: " + str(e))
+                    problem = True
+
+            if not problem:
                 smiles_to_3d_mol(smiles, ligand_preped)
-        except Exception as e:
-            print(ligand, e)
+                print("ligand preparation done")
+            else:
+                print(ref + " ligand preparation failed")
+
+    print("warnings/errors:")
+    print(sio.getvalue())
+    sys.stderr = stderr
--- modules/prepare_ligand_sdf.nf
+++ modules/prepare_ligand_sdf.nf
@@ -5,14 +5,17 @@
 params.OUTPUT = "$launchDir/data/ligands"
 
 process prepare_ligand_sdf {
-    publishDir(params.OUTPUT, mode: 'copy')
+    publishDir(params.OUTPUT, mode: 'copy', pattern: "*.sdf")
+    publishDir(params.OUTPUT, mode: 'copy', saveAs: { filename -> if (filename == ".command.log") "ligand_preparation.log"})
     conda '/scicore/home/schwede/leeman0000/miniconda3/envs/spyrmsd'
 
     input:
     path (ref_sdf_files)
+    path(mol_files)
 
     output:
     path ("*.sdf"), emit: sdf_files
+    path (".command.log"), emit: ligand_prep_log
 
     script:
     """
